d8ba1edfca8d6aa2e0fe67603aa6fdd01663e21d
Sharan N <sharan.n@asint.net>
2026-04-21 16:26:23 +0000

    init

---
+++ project1/webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"com/asint/project1/test/unit/controller/View1.controller"
+], function () {
+	"use strict";
+});
---
+++ project2/webapp/controller/App.controller.js
@@ -0,0 +1,10 @@
+sap.ui.define([
+  "sap/ui/core/mvc/Controller"
+], (BaseController) => {
+  "use strict";
+
+  return BaseController.extend("com.asint.project2.controller.App", {
+      onInit() {
+      }
+  });
+});
