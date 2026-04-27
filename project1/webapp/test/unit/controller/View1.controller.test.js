1a97e65a49f933c08759cf34d1678c2500cd1c98
Sharan N <sharan.n@asint.net>
2026-04-27 13:42:12 +0530

    CI workflow

--- project1/webapp/test/unit/controller/View1.controller.test.js
+++ project1/webapp/test/unit/controller/View1.controller.test.js
@@ -208,4 +208,12 @@ sap.ui.define([
 		assert.strictEqual(oController.something3(undefined, "second"), "Invalid", "Undefined first parameter returns Invalid");
 	});
 
+	QUnit.test("something1 should concatenate strings with & sign", function (assert) {
+		var oController = new Controller();
+		assert.strictEqual(oController.something4("hello", "world"), "hello&world", "Two strings concatenated with +");
+		assert.strictEqual(oController.something4("test", "123"), "test&123", "String and number concatenated");
+		assert.strictEqual(oController.something4("a", "b"), "a&b", "Single characters concatenated");
+		assert.strictEqual(oController.something4("first", undefined), "first&undefined", "Second parameter undefined");
+	});
+
 });
